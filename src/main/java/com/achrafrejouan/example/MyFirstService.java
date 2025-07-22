dd5edc8f5f5866a16eb7993f085bcdf12f55c105
achrafRejouan <achrafrejouan@gmail.com>
2025-07-22 19:05:47 +0100

    Try Field injection

--- src/main/java/com/achrafrejouan/example/MyFirstService.java
+++ src/main/java/com/achrafrejouan/example/MyFirstService.java
@@ -1,16 +1,13 @@
 package com.achrafrejouan.example;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
-    private final MyFirstClass myFirstClass;
-
-    public MyFirstService(@Qualifier("bean2") MyFirstClass myFirstClass) {
-        this.myFirstClass = myFirstClass;
-    }
+    @Autowired
+    private MyFirstClass myFirstClass;
 
     public String tellAStory(){
         return "The dependency is saying : " + myFirstClass.sayHello();
