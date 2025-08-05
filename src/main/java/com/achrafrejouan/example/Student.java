4e4db358d59feba60f97de635e988523d76f341f
achrafRejouan <achrafrejouan@gmail.com>
2025-08-05 05:52:16 +0100

    Refactor application configuration and add Student entity model

---
+++ src/main/java/com/achrafrejouan/example/Student.java
@@ -0,0 +1,66 @@
+package com.achrafrejouan.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Student {
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
