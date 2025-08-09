0e3b073aec932ac398940784458bb8432db2c38a
achrafRejouan <achrafrejouan@gmail.com>
2025-08-09 05:33:02 +0100

    Refactor SchoolController to use SchoolService; add SchoolService and SchoolMapper for improved separation of concerns

--- src/main/java/com/achrafrejouan/example/SchoolController.java
+++ src/main/java/com/achrafrejouan/example/SchoolController.java
@@ -1,42 +1,26 @@
 package com.achrafrejouan.example;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SchoolController {
-    private final SchoolRepository schoolRepository;
+    private final SchoolService schoolService;
 
-    public SchoolController(SchoolRepository schoolRepository) {
-        this.schoolRepository = schoolRepository;
+    public SchoolController(SchoolService schoolService) {
+        this.schoolService = schoolService;
     }
 
     @PostMapping("/schools")
-    public SchoolDto CreateSchool(@RequestBody SchoolDto school) {
-        var schoolDto = toSchool(school);
-        var savedSchool = schoolRepository.save(schoolDto);
-        return school;
-    }
-
-    private School toSchool(SchoolDto dto) {
-        return new School(dto.name());
-    }
-
-    private SchoolDto toSchoolDto(School school) {
-        return new SchoolDto(school.getName());
+    public SchoolDto createSchool(@RequestBody SchoolDto school) {
+        return schoolService.createSchool(school);
     }
 
     @GetMapping("/schools")
     public List<SchoolDto> findAllSchools() {
-        return schoolRepository.findAll()
-                .stream()
-                .map(this::toSchoolDto)
-                .collect(Collectors.toList());
+        return schoolService.findAllSchools();
     }
 }
---
+++ src/main/java/com/achrafrejouan/example/SchoolService.java
@@ -0,0 +1,31 @@
+package com.achrafrejouan.example;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class SchoolService {
+    private final SchoolMapper schoolMapper;
+    private final SchoolRepository schoolRepository;
+
+    public SchoolService(SchoolMapper schoolMapper, SchoolRepository schoolRepository) {
+        this.schoolMapper = schoolMapper;
+        this.schoolRepository = schoolRepository;
+    }
+
+    public SchoolDto createSchool(SchoolDto school) {
+        var schoolDto = schoolMapper.toSchool(school);
+        var savedSchool = schoolRepository.save(schoolDto);
+        return school;
+    }
+
+    public List<SchoolDto> findAllSchools() {
+        return schoolRepository.findAll()
+                .stream()
+                .map(schoolMapper::toSchoolDto)
+                .collect(Collectors.toList());
+    }
+}
